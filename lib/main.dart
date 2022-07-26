747e9b203781d274880844b3a693066b93e77414
miradouro <rafaelmiradouro@gmail.com>
2022-07-26 14:37:13 -0300

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'models/product_list.dart';
-import 'pages/counter_page.dart';
 import 'pages/product_detail_page.dart';
 import 'pages/products_overview_page.dart';
 import 'utils/app_routes.dart';
-import 'providers/counter.dart';
 
 void main() {
   runApp(MyApp());
---
+++ lib/models/cart.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+import 'cart_item.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  double get totalAmount{
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItemm(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (exintingItem) => CartItem(
+          id: exintingItem.id,
+          productId: exintingItem.productId,
+          name: exintingItem.name,
+          quantity: exintingItem.quantity + 1,
+          price: exintingItem.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          productId: product.id,
+          name: product.name,
+          quantity: 1,
+          price: product.price,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
+}
